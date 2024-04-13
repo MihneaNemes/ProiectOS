08ad3e54447d2f07bff22a32fa82eee03d027e03
KartofSnitel <kartofsnitel@Ubuntu.myguest.virtualbox.org>
2024-04-13 17:53:51 +0300

    second commit

--- proiect.c
+++ proiect.c
@@ -10,11 +10,10 @@
 
 struct EntryMetadata {
     char name[MAX_PATH_LENGTH];
-    char type; 
+    char type;
     time_t last_modified;
-    int size; 
+    int size;
 };
-
 
 void captureMetadata(const char *dir_path, FILE *snapshot_file) {
     struct dirent *entry;
@@ -44,12 +43,12 @@ void captureMetadata(const char *dir_path, FILE *snapshot_file) {
                 metadata.type = 'F';
             else
                 continue;
-            
+
             metadata.last_modified = file_stat.st_mtime;
             metadata.size = file_stat.st_size;
 
-            fprintf(snapshot_file, "%s\t%c\t%ld\t%lld\n", metadata.name, metadata.type, 
-                    (long)metadata.last_modified, (long long)metadata.size);
+            fprintf(snapshot_file, "%s\t%c\t%lld\t%s\n", metadata.name, metadata.type,
+                    (long long)metadata.size, ctime(&metadata.last_modified));
 
             if (metadata.type == 'D') {
                 captureMetadata(full_path, snapshot_file);
@@ -59,27 +58,58 @@ void captureMetadata(const char *dir_path, FILE *snapshot_file) {
     closedir(dir);
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <directory_path>\n", argv[0]);
-        return 1;
-    }
-
+void updateSnapshot(const char *dir_path, const char *output_dir) {
     char snapshot_path[MAX_PATH_LENGTH];
-    snprintf(snapshot_path, sizeof(snapshot_path), "%s/Snapshot.txt", argv[1]);
-    FILE *snapshot_file = fopen(snapshot_path, "w");
+    snprintf(snapshot_path, sizeof(snapshot_path), "%s/Snapshot.txt", output_dir);
+
+    FILE *snapshot_file = fopen(snapshot_path, "a");
     if (snapshot_file == NULL) {
-        perror("Unable to create snapshot file");
-        return 1;
+        perror("Unable to open snapshot file for appending");
+        return;
     }
 
-    fprintf(snapshot_file, "The order is: Name, Type, Last Modified, Size\n");
-    captureMetadata(argv[1], snapshot_file);
+    
+    fseek(snapshot_file, 0, SEEK_END);
+    long file_size = ftell(snapshot_file);
+    if (file_size == 0) {
+        fprintf(snapshot_file, "The order is: Name, Type, Last Modified, Size\n");
+    }
+    else {
+        fseek(snapshot_file, file_size - 1, SEEK_SET); 
+        char last_char = fgetc(snapshot_file);
+        if (last_char != '\n') {
+            fprintf(snapshot_file, "\n"); 
+        }
+    }
+
+    captureMetadata(dir_path, snapshot_file);
 
     fclose(snapshot_file);
-    printf("Snapshot captured successfully!\n");
-
-    return 0;
+    printf("Snapshot for directory %s captured successfully!\n", dir_path);
 }
 
 
+int main(int argc, char *argv[]) {
+    if (argc < 2 || argc > 11) {
+        printf("Usage: %s -o <output_dir> <dir1> <dir2> ... <dirN>\n", argv[0]);
+        return 1;
+    }
+
+    char *output_dir = NULL;
+    int i;
+    for (i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-o") == 0) {
+            if (i + 1 < argc) {
+                output_dir = argv[i + 1];
+                i++;  
+            } else {
+                printf("Missing output directory path after -o option.\n");
+                return 1;
+            }
+        } else {
+            updateSnapshot(argv[i], output_dir);
+        }
+    }
+
+    return 0;
+}
